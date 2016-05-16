5b1200058b1561548d9c7a6761901a45ea4a1557
Beyonce Knowles <asch.harwood@gmail.com>
2016-05-16 08:29:10 -0400

    initial commit

--- weather.py
+++ weather.py
@@ -22,19 +22,26 @@ cur = con.cursor()
 
 with con:
 	cur.execute("DROP TABLE IF EXISTS daily_max_temp")
-	con.execute('CREATE TABLE daily_max_temp (daytime TEXT, dailymaxtemp INT)')
+	con.execute('CREATE TABLE daily_max_temp (daytime TEXT, city1 REAL, city2 REAL, city3 REAL, city4 REAL, city5 REAL)')
 
 """loop to iterate days"""
 
+cities = { "Atlanta": '33.762909,-84.422675',
+            "Austin": '30.303936,-97.754355',
+            "Boston": '42.331960,-71.020173',
+            "Chicago": '41.837551,-87.681844',
+            "Cleveland": '41.478462,-81.679435'
+        }
 dayweather = 0
 for dayweather in range(30):
-	cities_weather = requests.get('https://api.forecast.io/forecast/e967d7c3d78c3979655d077839f5c60c/40.680991,-73.998259, %s' % start_datestr)
-	df = json_normalize(cities_weather.json()['hourly']['data']) #need to set index as time
-	df_indexed= df.set_index(df['time'])
-	dailymaxtemp = max(df_indexed['temperature'])
-	dailymaxtempidx=df_indexed['temperature'].idxmax()
+	for city in cities.values():
+		cities_weather = requests.get('https://api.forecast.io/forecast/e967d7c3d78c3979655d077839f5c60c/%s, %s' % (city, start_datestr))
+		df = json_normalize(cities_weather.json()['hourly']['data']) #need to set index as time
+		df_indexed= df.set_index(df['time'])
+		dailymaxtemp = max(df_indexed['temperature'])
+		dailymaxtempidx=df_indexed['temperature'].idxmax()
 	with con:
-		cur.execute('INSERT INTO daily_max_temp(daytime, dailymaxtemp) VALUES (?,?)', (dailymaxtempidx, dailymaxtemp))
+			cur.execute('INSERT INTO daily_max_temp(daytime, dailymaxtemp) VALUES (?,?)', (dailymaxtempidx, city))
 	#insert SQL update statement here that grabs 
 	dayweather += 1
 	start_date = start_date + datetime.timedelta(days=1)
---
+++ weather2.py
@@ -0,0 +1,13 @@
+import requests
+import sqlite3 as lite
+import datetime
+
+api_key = "<api_key>"
+url = 'https://api.forecast.io/forecast/' + api_key
+
+cities = { "Atlanta": '33.762909,-84.422675',
+            "Austin": '30.303936,-97.754355',
+            "Boston": '42.331960,-71.020173',
+            "Chicago": '41.837551,-87.681844',
+            "Cleveland": '41.478462,-81.679435'
+        }
